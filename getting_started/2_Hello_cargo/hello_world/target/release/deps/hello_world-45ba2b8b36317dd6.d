f77d493dd6caabc318c2df1b63ca7fc374d7bda2
Sachin6624 <sachin.murali@firstwave.sg>
2023-08-29 19:50:34 +0530

    hello cargo completed

---
+++ getting_started/2_Hello_cargo/hello_world/target/release/deps/hello_world-45ba2b8b36317dd6.d
@@ -0,0 +1,5 @@
+/Users/sachinmurali/Desktop/Rustaceans/getting_started/2_Hello_cargo/hello_world/target/release/deps/hello_world-45ba2b8b36317dd6: src/main.rs
+
+/Users/sachinmurali/Desktop/Rustaceans/getting_started/2_Hello_cargo/hello_world/target/release/deps/hello_world-45ba2b8b36317dd6.d: src/main.rs
+
+src/main.rs:
