17a65f1f349079e08efa0c0312a1e675d9c4439e
Sachin6624 <sachin.murali@firstwave.sg>
2023-08-29 18:49:36 +0530

    hello world created

---
+++ hello_world/target/debug/deps/hello_world-443cedf9f79fc0ac.d
@@ -0,0 +1,5 @@
+/Users/sachinmurali/Desktop/Rustaceans/hello_world/target/debug/deps/hello_world-443cedf9f79fc0ac.rmeta: src/main.rs
+
+/Users/sachinmurali/Desktop/Rustaceans/hello_world/target/debug/deps/hello_world-443cedf9f79fc0ac.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/deps/hello_world-ce3014f012ed58a4.d
@@ -0,0 +1,5 @@
+/Users/sachinmurali/Desktop/Rustaceans/hello_world/target/debug/deps/hello_world-ce3014f012ed58a4.rmeta: src/main.rs
+
+/Users/sachinmurali/Desktop/Rustaceans/hello_world/target/debug/deps/hello_world-ce3014f012ed58a4.d: src/main.rs
+
+src/main.rs:
