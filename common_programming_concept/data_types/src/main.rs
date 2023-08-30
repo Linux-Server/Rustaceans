688cc71c00bd666e014feb920ac150939cbda57a
Sachin6624 <sachin.murali@firstwave.sg>
2023-08-30 16:19:09 +0530

    data types

---
+++ common_programming_concept/data_types/src/main.rs
@@ -0,0 +1,21 @@
+/*
+//DATA TYPES
+1.Every value in rust is  a certain data type
+2.data types are mainly two, scalar and compound data types
+3.rust is statically typed language
+4.it must know all the varibales at compile time
+5.the compiler can usually infer data type based on the value we assign ti it
+6. In some cases, developer need to annotate type, where compiler is unable to infer the data type
+
+//SCALAR TYPES
+
+1.
+
+
+*/
+
+
+fn main() {
+    // here we need to give an explicit type for guess variables
+   let guess = "42".parse().expect("Unable to parse");
+}
