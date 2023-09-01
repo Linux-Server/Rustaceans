e9a7bf74686849528431b8eb7b4ae0f8728df19f
Sachin6624 <sachin.murali@firstwave.sg>
2023-09-01 17:09:42 +0530

    lifetimes 1st check

---
+++ lifetimes/target/debug/deps/lifetimes-edbd6acbfabb3bb3.d
@@ -0,0 +1,5 @@
+/Users/sachinmurali/Desktop/Rustaceans/lifetimes/target/debug/deps/lifetimes-edbd6acbfabb3bb3: src/main.rs
+
+/Users/sachinmurali/Desktop/Rustaceans/lifetimes/target/debug/deps/lifetimes-edbd6acbfabb3bb3.d: src/main.rs
+
+src/main.rs:
---
+++ lifetimes/target/debug/lifetimes.d
@@ -0,0 +1,1 @@
+/Users/sachinmurali/Desktop/Rustaceans/lifetimes/target/debug/lifetimes: /Users/sachinmurali/Desktop/Rustaceans/lifetimes/src/main.rs
