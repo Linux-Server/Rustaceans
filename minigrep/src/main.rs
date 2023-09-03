015fcf4bc65c65a0476b1777b185032415917741
Sachin6624 <sachin.murali@firstwave.sg>
2023-09-03 13:04:42 +0530

    code refractoring in config struct

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,17 +6,30 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
     println!("Args : {:?}", args);  // args[0] is the anme of the program
-
-    let query = &args[1];
-    let file_path = &args[2];
-//cargo run -- sam ram
-    println!("Query passed : {:?}", query);  
-    println!("File name : {:?}", file_path);
+    let config= parse_config(&args);
+   
+    //cargo run -- sam ram
+    println!("Query passed : {:?}", config.query);  
+    println!("File name : {:?}", config.file_path);
 
     //open the file and read string
-    let contents = fs::read_to_string("poem.txt");
+    // this should be error handled properly
+    let contents = fs::read_to_string("poem.txt").expect("Unable to read file");
 
     println!("The contents : {:?}", contents);
 
 
+}
+
+
+fn parse_config(args: &[String])->Config{
+    let query = args[1].clone();
+    let file_path = args[2].clome();
+    Config{query,file_path}
+
+}
+
+struct Config{
+    query:String,
+    file_path: String
 }
