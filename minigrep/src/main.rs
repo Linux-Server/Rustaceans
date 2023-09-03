5808e7a39dc1e26dcfe4ec1d43465f7c582407b3
Sachin6624 <sachin.murali@firstwave.sg>
2023-09-03 14:59:19 +0530

    extraction of function from binary crate into library create

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
+use minigrep::run;
 
 
 fn main() {
@@ -25,28 +26,3 @@ fn main() {
 
 }
 
-fn run(config:Config)-> Result<(), Box<dyn Error>>{
-    //open the file and read string
-    // this should be error handled properly
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("The contents : {:?}", contents);
-    Ok(())
-}
-
-
-
-
-struct Config{
-    query:String,
-    file_path: String
-}
-
-impl Config{
-    fn build(args: &[String])->Result<Config,&str>{
-        if args.len() < 3 {
-            return Err("not enogh args")
-        }
-        Ok(Config { query: args[1].clone(), file_path: args[2].clone() })
-    }
-}
