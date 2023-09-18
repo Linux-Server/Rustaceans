0c38b2468168fe55c916625b99b17849caca0391
Sachin <sachin6624@gmail.com>
2023-09-18 23:23:54 +0530

    trait object check

---
+++ trait_object/src/lib.rs
@@ -0,0 +1,15 @@
+// pub trait Draw{
+//     fn draw(&self);
+// }
+
+// pub struct Screen<T>{
+//     pub components: Vec<Box<T>> // instead of Vec<T>
+// }
+
+// impl<T> Screen<T>{
+//     fn run(&self){
+//         for comp in self.components.iter(){
+//             comp.draw()
+//         }
+//     }
+// }
