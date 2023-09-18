72bc1d1eec856826826b4a428191e24fbf67ae15
Sachin <sachin6624@gmail.com>
2023-09-18 23:49:32 +0530

    tuple vector check

--- trait_object/src/bin/box.rs
+++ trait_object/src/bin/box.rs
@@ -3,7 +3,17 @@ fn main(){
     let a = Box::new(3);
     println!("The value of a is {:?}", a);
     assert_eq!(3, *a); // deref required , otherwise assertion will fail
+
+    // check Box with vector
+
+    Myvector.0.push(20)
+
+    //  my_vector.push(a);
+    //  my_vector.push(3)
 }
+
+
+struct Myvector<T>(Vec<T>);
 
 /*
 
