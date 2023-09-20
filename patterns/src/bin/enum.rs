e2580bee4df7e9c596352d8795be5aa44721213f
Sachin <sachin6624@gmail.com>
2023-09-20 23:11:39 +0530

    nested enum destructure

--- patterns/src/bin/enum.rs
+++ patterns/src/bin/enum.rs
@@ -1,12 +1,22 @@
 fn main(){
 
-    let msg = Message::ChangeColor(10, 20, 30);
-
+    let msg = Message::Quit;
     match msg{
         Message::Quit => println!("Its a quit situation"),
         Message::Move { x, y } => println!("Its a move"),
-        Message::ChangeColor(r, g, b) => println!("Ist a rgb"),
-        Message::Write(val) =>  println!("Its a string")
+        Message::Write(val) =>  println!("Its a string"),
+        _ => ()
+
+    }
+
+    let msg = Message::ChangeColor(Color::Hsv(10, 20, 30));
+
+    match msg{
+        Message::ChangeColor(Color::Hsv(x, y, z)) =>{
+            println!("The vals are {x}, {y},{z}");
+        }
+        _ => ()
+            
     }
 
 }
@@ -15,6 +25,11 @@ enum Message{
     Quit,
     Move { x: i32, y: i32 },
     Write(String),
-    ChangeColor(i32, i32, i32),
+    ChangeColor(Color),
 
+}
+
+enum Color{
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
 }
