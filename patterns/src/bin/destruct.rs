a4f200c6a78d528e51ebe3b40df5bab8b4bd10ba
Sachin <sachin6624@gmail.com>
2023-09-20 19:37:35 +0530

    pattern match for struct destruct

--- patterns/src/bin/destruct.rs
+++ patterns/src/bin/destruct.rs
@@ -1,12 +1,22 @@
 fn main(){
     let person = Person{name:"sachin".to_string()};
-
+    // // bighand
     let Person {name: sam} = person;
 
     println!("the destruct is {:?}", sam);
 
+    //sugar syntax
+    let person1 = Person1{name:"sachin".to_string()};
+
+    let Person1 {name} = person1;
+
 }
 #[derive(Debug)]
 struct Person{
+    name:String
+}
+
+#[derive(Debug)]
+struct Person1{
     name:String
 }
