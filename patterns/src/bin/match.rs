189445318af2bec019c4973c978b7a65979ff2d4
Sachin <sachin6624@gmail.com>
2023-09-20 19:08:29 +0530

    range matching

--- patterns/src/bin/match.rs
+++ patterns/src/bin/match.rs
@@ -5,4 +5,12 @@ fn main(){
         10|20 => println!("The value is 10 or 20"),  // or operator
         _ => println!("not the expected value")
     }
+
+    //range matching
+
+    match x{
+        1..=5 => println!("Not bwtwenn 1 to 4"),
+        9..=12 => println!("Got the expected value"),
+        _ => println!("Not found")
+        }
 }
