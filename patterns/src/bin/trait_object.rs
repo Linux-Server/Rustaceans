a106eda309c36ad9411b1c4c2166013631ee0afa
Sachin <sachin6624@gmail.com>
2023-09-21 12:49:24 +0530

    trait object

--- patterns/src/bin/trait_object.rs
+++ patterns/src/bin/trait_object.rs
@@ -1,9 +1,8 @@
 fn main(){
-    let inner_data = Box::new(10);
-    let inner_data1 = Box::new("hello".to_string());
-     let my_vector:Vec<Box<dyn Draw>> =vec![inner_data, inner_data1];
-
-     let data = Screen{ components: my_vector };
+    let inner_data = Box::new(Button{width:10, height:20, label: "login".to_string()});
+    let inner_data1 = Box::new(TextField{color:"red".to_string(), height:44, name: "Usename".to_string()});
+    let my_vector:Vec<Box<dyn Draw>>=vec![inner_data, inner_data1];
+    let data = Screen{ components: my_vector };
 
 }
 
@@ -14,13 +13,26 @@ struct Screen{
     components: Vec<Box<dyn Draw>>
 }
 
-impl Draw for i32{
+struct Button{
+    width: i32,
+    height:i32,
+    label: String
+}
+
+impl Draw for Button{
     fn draw(&self) {
-        
+        println!("Im drawing button")
     }
 }
-impl Draw for String{
+
+struct TextField{
+    color: String,
+    height:i32,
+    name: String
+}
+
+impl Draw for TextField{
     fn draw(&self) {
-        
+        println!("Im drawing button")
     }
 }
