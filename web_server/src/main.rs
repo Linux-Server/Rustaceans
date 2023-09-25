ce7619ddefb6f76b51889677b4eb24ef6e566ce9
Sachin <sachin6624@gmail.com>
2023-09-25 19:15:09 +0530

    return 200 status code

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -9,11 +9,11 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        handle_connection1(stream);
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection1(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let http_request: Vec<_> = buf_reader
         .lines()
@@ -21,5 +21,7 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {:#?}", http_request);
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
