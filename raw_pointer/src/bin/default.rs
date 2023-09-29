baa727df22670b2c3118e1557cc4a4420155c5b7
Sachin <sachin6624@gmail.com>
2023-09-29 12:52:36 +0530

    advanced traits

---
+++ raw_pointer/src/bin/default.rs
@@ -0,0 +1,27 @@
+use std::ops::Add;
+
+fn main(){
+   let x = Point{x:20,y:30} + Round{ x:80,y:70};
+   println!("Th eval {:?}", x);
+}
+#[derive(Debug)]
+struct Point{
+    x:i32,
+    y:i32
+}
+
+struct Round{
+    x:i32,
+    y:i32
+}
+
+
+impl Add<Round> for Point{
+    type Output = Point;
+    fn add(self, rhs: Round) -> Self::Output {
+        Point{
+            x: self.x + rhs.x,
+            y: self.y + rhs.y 
+        }
+    }
+}
