d16bfdfb13f2910c51f21dfec011df0278f1581f
Sachin <sachin6624@gmail.com>
2023-10-09 11:40:28 +0530

    retrun from closure

--- closure/src/bin/test3.rs
+++ closure/src/bin/test3.rs
@@ -1,5 +1,16 @@
-fn main(){
+use std::thread;
 
-    let x = 10;
-    assert!(x.is_negative())
+fn main(){
+  let vec1 = vec![1,2,3,4,5,6];
+
+  let x = move|| vec1;
+
+  let sam = x();
+
+  println!("{:?}", sam);
+  
 }
+
+// fn -> take refernec
+// fnmut -> take mut refrence
+//fnonce -> takes ownership
