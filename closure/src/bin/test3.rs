e316f4cf6d027180af406c4dcfe60d496deee767
Sachin <sachin6624@gmail.com>
2023-10-10 19:22:01 +0530

    sort by key

--- closure/src/bin/test3.rs
+++ closure/src/bin/test3.rs
@@ -3,9 +3,9 @@ use std::thread;
 fn main(){
   let vec1 = vec![1,2,3,4,5,6];
 
-  let x = move|| vec1;
+  let x = |x:i32| vec1;
 
-  let sam = x();
+  let sam = x(10);
 
   println!("{:?}", sam);
   
