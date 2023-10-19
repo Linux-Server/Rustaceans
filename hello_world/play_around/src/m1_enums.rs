ce5d387a87670a6957275ae1db2c87ccea67d995
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2023-10-19 19:37:20 +0530

    enum test cases

--- hello_world/play_around/src/m1_enums.rs
+++ hello_world/play_around/src/m1_enums.rs
@@ -1,14 +1,43 @@
-
-
-#[cfg(test)]
-#[test]
-fn my_dummy_test(){
-    assert!(true);
-    println!("Its a dummy test by sachin");
+#[allow(dead_code)]
+#[derive(Debug)]
+enum CarColor{
+    Red,
+    Blue,
+    Green
 }
 
-#[test]
-fn my_random_test(){
-    assert!(true);
-    println!("Its a random test by sachin");
+#[derive(Debug)]
+enum GivenResult<T,U>{
+    Ok(T),
+    Err(U)
+}
+
+fn create_car_color() -> CarColor{
+    let first_car = CarColor::Red;
+    first_car
+}
+
+fn check_under_five(num:i32)-> GivenResult<i32, &'static str>{
+    if num<5{
+        GivenResult::Ok(num)
+    }else{
+        GivenResult::Err("Not expected")
+    }
+}
+
+#[cfg(test)]
+
+mod test{
+   use super::*;
+    #[test]
+    fn test_car_color(){
+        let car_color = create_car_color();
+        dbg!(car_color);
+    }
+    #[test]
+    fn test_enums(){
+        let under_five = check_under_five(20);
+        dbg!(under_five);
+    }
+
 }
