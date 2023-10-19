8c92573cf1b6e06cb1748f6a7129f93374202b52
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2023-10-19 19:07:47 +0530

    dummy test

---
+++ hello_world/play_around/src/m1_enums.rs
@@ -0,0 +1,14 @@
+
+
+#[cfg(test)]
+#[test]
+fn my_dummy_test(){
+    assert!(true);
+    println!("Its a dummy test by sachin");
+}
+
+#[test]
+fn my_random_test(){
+    assert!(true);
+    println!("Its a random test by sachin");
+}
