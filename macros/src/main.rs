1b61c328306cf88544b89d23f93f9d71f395e44f
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2023-10-31 23:32:12 +0530

    cargo expand

---
+++ macros/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // println!("Hello, world!");
+
+    // println!("the person is {}", person_one.name());
+}
+
+#[derive(Clone,Copy)]
+struct Person {
+    name: u32,
+    age: u32,
+}
+
--- smart_pointer/src/bin/box1.rs
+++ smart_pointer/src/bin/box1.rs
@@ -4,4 +4,18 @@ fn main() {
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
+
+    //Every single value has an owner
+    // there can be only one owner at a time
+    // when variable goes out of scope, value will be dropped
+
+    {
+   
+    let y = String::from("hello");   // smart poineter, len, capacity (string )
+
+    let b = y; // shallow copy
+    // let b = y.clone() // deep copy
+    }
+
+
 }
