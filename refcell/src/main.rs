d7be001cc44c1c9bca67ae9ed55c6a7646ef1fd5
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2023-11-09 18:30:23 +0530

    test error

--- refcell/src/main.rs
+++ refcell/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     println!("Hello, world!");
 }
 
-trait Messager{
+trait Messenger{
     fn send(&self, message:&str);
 }
 
@@ -13,7 +13,7 @@ struct LimitTracker<'a, T>{
     max:usize
 }
 
-impl<'a,T: Messager> LimitTracker<'a,T> {
+impl<'a,T: Messenger> LimitTracker<'a,T> {
       pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
         LimitTracker {
             messenger,
@@ -38,4 +38,39 @@ impl<'a,T: Messager> LimitTracker<'a,T> {
         }
     }
 
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct MockMessenger {
+        sent_messages: Vec<String>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: vec![],
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_messages.len(), 1);
+    }
 }
