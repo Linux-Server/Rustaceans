bf83759ccd8813ed8629f3a7cf031baf08693d2a
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2023-11-12 16:32:29 +0530

    gcd

---
+++ o-reilly/src/main.rs
@@ -0,0 +1,26 @@
+fn main(){
+   //Euclids Algorithm
+   let x:u128 =400000000000000000000000000000000000;
+   let y: u128 = 10000000000000000000000000000;
+   let res = gcd(x,y);
+   println!("The greatest common divisor {:?}", res);
+
+}
+
+fn gcd(mut x:u128,mut y:u128)->u128{
+   //greatest common divisor
+   //a greatet number which can divide both without causing reminder
+   while y !=0{
+    if y<x{
+      let t=x;
+      x = y; 
+      y =t;
+   }
+    y = y%x;
+    println!("Then {:?} {:?}", y, x);
+} 
+
+   return x;
+
+
+}
