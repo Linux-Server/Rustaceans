983d5b21cc2cb53a46e9202c92ded1d4ba86aafa
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2023-11-21 19:49:00 +0530

    Rust Rover - An IDE for Rust Developers

---
+++ fundamental_types/src/main.rs
@@ -0,0 +1,19 @@
+#![allow(unused_variables, unused_assignments)]
+fn main() {
+   let res = sam(10);
+   let out = res;
+   println!("The result is {:?}", out);
+
+
+
+}
+
+fn sam(num: i32)-> i32{
+   let b = num;
+   let c =10;
+   let mut x =0;
+   for i in 0..10{
+      x = i;
+   }
+   b
+}
