c76d8cc289276ce425ddeea6baf359b9f8f6ed1e
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2023-12-11 22:19:43 +0530

    fully qualified syntax -phase 3

--- trait_advanced/src/bin/fully.rs
+++ trait_advanced/src/bin/fully.rs
@@ -6,28 +6,35 @@ fn main() {
     let b = <Dog as Animal>::baby();
     println!("The b is {:?}", b);
 
+    let c = Dog.baby1();
+
+    let d = Animal::baby1(&Dog);
+
 }
 
 struct Dog;
 
 trait Animal{
     fn baby()->String;
+    fn baby1(&self);
 }
 
 impl Dog{
     fn baby()->String{
         format!("Homw baby")
 }
+    fn baby1(&self){
+        println!("Its a self home baby");
+    }
 }
 
 impl Animal for Dog{
     fn baby() -> String {
         format!("Animal baby")
     }
-}
 
-impl Animal for i32{
-    fn baby() -> String {
-        format!("kiler")
+    fn baby1(&self) {
+        println!("Its a animal self");
     }
+    
 }
