9dd3585ab58575cdc24fd8e656dbe39df9b1bed4
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2023-12-11 21:34:41 +0530

    fully qualified syntax -phase 2

--- trait_advanced/src/bin/qualified_syntax.rs
+++ trait_advanced/src/bin/qualified_syntax.rs
@@ -1,3 +1,47 @@
 fn main() {
     println!("fully qaulified syntax");
+    let human = Human::new();
+    let c = Human::run(&human);
+    // both are same , but 2nd one preferred
+    println!("The human {:?}", Human::run(&human));
+    println!("The human {:?}", human.run());
+
+    let hum_wizard = Wizard::run(&human);
+
+
+}
+#[derive(Debug)]
+struct Human{
+    id:i32
+}
+
+impl Human{
+    fn run(&self){
+        println!("human can run");
+    }
+
+    fn new()->Human{
+        Human{
+            id:1
+        }
+    }
+}
+
+impl Pilot for Human{
+    fn run(&self) {
+        println!("Pilot can be human");
+    }
+}
+
+impl Wizard for Human{
+    fn run(&self) {
+        println!("Wizard can be human");
+    }
+}
+
+trait Pilot{
+    fn run(&self);
+}
+trait Wizard{
+    fn run(&self);
 }
