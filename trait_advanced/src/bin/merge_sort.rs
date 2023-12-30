2279fd56becb42a583a0ac397ec37243ad5f7a8f
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2023-12-30 19:24:59 +0530

    closure

---
+++ trait_advanced/src/bin/merge_sort.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let mut a = vec![10,11,12,13,14,1,2,3,4,5];
+    let mut aux = Vec::<i32>::new();
+    for (k,val) in a.iter().enumerate(){
+        aux.push(*val)
+    }
+    let lo = a[0]; //10
+    let mid = a[4]; //14
+    let hi = a[a.len()-1];//5
+
+    for (k,val) in aux.iter().enumerate(){
+        a[]
+
+    }
+    //a[0] = 100;
+
+    println!("The data {:?} and {:?} and {:?}", lo, hi, mid);
+    // println!("The val : {:?}", aux);
+    // println!("The val : {:?}", a);
+
+
+
+}
