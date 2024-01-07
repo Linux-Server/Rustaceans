e1defcd78884f5edeb7d5bbfbce71a5bdd723606
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-01-07 20:56:42 +0530

    copied and cloned

--- workspace/server/src/bin/copied.rs
+++ workspace/server/src/bin/copied.rs
@@ -7,4 +7,16 @@ fn main() {
 
     println!("The copy is {:?}", x_copied);
 
+    // count the number of elements
+
+    println!("The count is {:?}", x_copied.iter().count());
+    println!("The last element in vector is {:?}", x_copied.iter().last());
+
+    let x = vec!["sam".to_string(),"ram".to_string()];
+    let x_iter = x.iter();
+    let y = x_iter.cloned().collect::<Vec<String>>();
+    println!("The value is : {:?}", y);
+    println!("The original is : {:?}", x);
+
+
 }
