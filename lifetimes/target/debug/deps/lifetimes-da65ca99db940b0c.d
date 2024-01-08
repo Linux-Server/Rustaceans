2b6407539a9890362c0702075b9a95fe1619ca92
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-01-08 22:31:23 +0530

    Delete lifetimes/target directory

--- lifetimes/target/debug/deps/lifetimes-da65ca99db940b0c.d
+++
@@ -1,5 +0,0 @@
-/Users/sachinmurali/Desktop/Rustaceans/lifetimes/target/debug/deps/lifetimes-da65ca99db940b0c.rmeta: src/main.rs
-
-/Users/sachinmurali/Desktop/Rustaceans/lifetimes/target/debug/deps/lifetimes-da65ca99db940b0c.d: src/main.rs
-
-src/main.rs:
--- lifetimes/target/debug/deps/lifetimes-edbd6acbfabb3bb3.d
+++
@@ -1,5 +0,0 @@
-/Users/sachinmurali/Desktop/Rustaceans/lifetimes/target/debug/deps/lifetimes-edbd6acbfabb3bb3: src/main.rs
-
-/Users/sachinmurali/Desktop/Rustaceans/lifetimes/target/debug/deps/lifetimes-edbd6acbfabb3bb3.d: src/main.rs
-
-src/main.rs:
